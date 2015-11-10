3ec583c4ef7204c3d9c2bbc89d97d7fe44fdcabc
Wisney <wisneytaas@gmail.com>
2015-11-10 10:43:27 -0200

    Propriedades do Aluno Finalizadas
    Add
    Edita
    Remove
    Lista

--- app.js
+++ app.js
@@ -33,3 +33,38 @@ app.controller('impostoController', function() {
     if (salario > 3500 ) { return salario*0.3};
   };
 });
+
+app.controller('reviewController', function() {
+    this.texto = "Comentario";
+    this.estrelas = 3;
+    this.vetor = [];
+
+    this.addvetor = function(text,estrela){
+      var comit = { comentario:text,estrelas:estrela };
+      this.vetor.push(comit);
+
+      this.texto = "New Comentario";
+      this.estrelas = 3;
+    }
+});
+
+app.controller('contactController', function() {
+    this.nome = "João";
+    this.telefone = "(31) 9988-7766";
+    this.email = "exemplo@exemplo.com";
+    this.vetor = [];
+
+    this.add = function(){
+      var contato = { nome:this.nome,telefone:this.telefone,email:this.email };
+      this.vetor.push(contato);
+      this.nome = "João";
+      this.telefone = "(31) 9988-7766";
+      this.email = "exemplo@exemplo.com";
+    }
+    this.remove = function(id){
+      if (id > -1) {
+          this.vetor.splice(id, 1);
+      }
+    }
+
+});
